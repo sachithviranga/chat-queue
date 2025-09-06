416326fea1717961dd45b1ee7a4b9bd9729a1a2a
Sac <sachith.viranga@gmail.com>
2025-09-06 20:18:56 +0800

    changes

--- BackEnd/ChatQueue.API/Program.cs
+++ BackEnd/ChatQueue.API/Program.cs
@@ -27,8 +27,6 @@ try
 
     var builder = WebApplication.CreateBuilder(args);
 
-    // Add services to the container.
-
     builder.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
