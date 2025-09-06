cb4158164ee2fedef936b813c257c0d27b1b007f
Sac <sachith.viranga@gmail.com>
2025-09-06 19:28:46 +0800

    added versioning

--- BackEnd/ChatQueue.API/Controllers/ChatController.cs
+++ BackEnd/ChatQueue.API/Controllers/ChatController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ChatQueue.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class ChatController : ControllerBase
     {
